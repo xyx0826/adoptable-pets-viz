822b503e1a63f5c6ab4a3419c64c1fb0bcb55b6d
xyx0826 <xyx0826@hotmail.com>
2021-06-02 20:59:34 -0700

    Create inspector table scaffold

--- server.R
+++ server.R
@@ -8,19 +8,44 @@
 #
 
 library(shiny)
+library(DT)
+
+source("scripts/utils.R")
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+shinyServer(function (input, output) {
+    # Load data and create static titles
+    df <- getData()
+    output$inspectorTitle = renderUI({
+        tags$h3(paste("Get to know the", nrow(df), "pets for adoption in Montgomery County, MD."))
     })
-
+    output$explorerTitle = renderUI({
+        tags$h3("Quick summaries of Montgomery County, MD's adoptable pets.")
+    })
+    
+    # Inspector selected info
+    selectedRow <- reactive({
+        input$inspectorTable_rows_selected
+    })
+    output$inspectorSelected = renderText(
+        if (length(selectedRow()) == 0) "Select a row to view its details." else paste("Selected row:", selectedRow())
+    )
+    
+    # Inspector table
+    output$inspectorTable = renderDT({
+        df %>%
+            rename(ID = Animal.ID) %>%
+            rename("Intake type" = Intake.Type) %>%
+            rename("Intake date" = In.Date) %>%
+            rename(Name = Pet.Name) %>%
+            rename(Species = Animal.Type) %>%
+            rename(Size = Pet.Size) %>%
+            rowwise() %>%
+            mutate(Age = round(Pet.Age, digits = 2)) %>%
+            mutate(Color = paste(Color, collapse = " / ")) %>%
+            as.data.frame() %>%
+            select(-Pet.Age, -URL.Link, -Crossing)
+        },
+        selection = "single"
+    )
 })
